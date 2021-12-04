ea7b389bc111b53d6d2ee39bd7c9f8ba7bc09258
temich <temich12107@gmail.com>
2021-12-04 20:56:43 +0300

    Update CallbackqueryCommand.php
    test number

--- bot/Commands/CallbackqueryCommand.php
+++ bot/Commands/CallbackqueryCommand.php
@@ -177,7 +177,7 @@ class CallbackqueryCommand extends SystemCommand
         //  $text .= "<b>Сделайте выбор</b>".PHP_EOL;
 
         foreach ($all_contacts as $value) {
-        $text .= $value ['name'].PHP_EOL;
+        $text .= $value ['name'].$value ['number'] .PHP_EOL;
         }
 
           $inline_keyboard = new InlineKeyboard([
