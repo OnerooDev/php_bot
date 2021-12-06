9af4543d08c0cbe5c27797a6424278d7c5aba3d0
temich <temich12107@gmail.com>
2021-12-06 10:21:00 +0300

    Update CallbackqueryCommand.php
    test

--- bot/Commands/CallbackqueryCommand.php
+++ bot/Commands/CallbackqueryCommand.php
@@ -177,7 +177,7 @@ class CallbackqueryCommand extends SystemCommand
         //  $text .= "<b>Сделайте выбор</b>".PHP_EOL;
 
         foreach ($all_contacts as $value) {
-        $text .= $value ['name'] .$value ['position'] . $value ['number']
+        $text .= $value ['name'] "." .$value ['position'] . $value ['number']
         .PHP_EOL;
         }
 
