339b804f01d238c4db3a6489f7415068fd0b9e44
temich <temich12107@gmail.com>
2022-02-07 15:18:22 +0300

    Update StartCommand.php
    t

--- bot/Commands/StartCommand.php
+++ bot/Commands/StartCommand.php
@@ -84,7 +84,7 @@ class StartCommand extends SystemCommand
 
 		    $mysqli = new \mysqli($this->config->host, $this->config->user, $this->config->password, $this->config->db);
         $mysqli->query("SET NAMES 'utf8'");
-        $query = "SELECT * FROM `Contacts` where `id_user` = `id`";
+//        $query = "SELECT * FROM `Contacts` where `id_user` = `id`";
 
 // Добавим проверку входящих запросов к команде Start по user_id == 'твой id'
 //        if ($user_id) {If ($user_id == "712276763" )
